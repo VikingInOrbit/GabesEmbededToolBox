ed84850cf396293741e31b89dfdb91082bc616e7
VikingInOrbit <104018760+VikingInOrbit@users.noreply.github.com>
2025-11-10 15:40:49 +0100

    doc

--- platform/arduino/serial_adapter.cpp
+++ platform/arduino/serial_adapter.cpp
@@ -3,4 +3,6 @@
 
 void serialStart(unsigned long baud) { Serial.begin(baud); }
 void serialStop() { Serial.end(); }
+//bool serialTest() { if(Serial){return true;} } //is started bool
 void serialWrite(const char* msg) { Serial.print(msg); }
+void serialRead(const char* msg) { Serial.read(); }
